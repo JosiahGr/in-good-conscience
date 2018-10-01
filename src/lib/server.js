45b3e425c78a9d15d3a4f87e69797c06074ffa47
Josiah Green <JosiahGreen90@gmail.com>
2018-10-01 10:52:39 -0700

    working on server bug with mongo

--- src/lib/server.js
+++ src/lib/server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import logger from './logger';
+import errorMiddleware from './error-middleware';
 
 const app = express();
 let server = null;
